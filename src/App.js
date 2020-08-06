dc910672ab1142834fedc5169b20c40f3f905648
Ruslan Robotko <42251945+robotkoruslan@users.noreply.github.com>
2020-08-06 11:55:06 +0300

    Before force flag
    Before force flag

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 
 import React, { Component } from 'react';
-import { Table, Button } from 'reactstrap';
+import { Modal, ModalHeader, FormGroup, ModalFooter, Label, Input, ModalBody, Table, Button } from 'reactstrap';
 import axios from 'axios';
 
 
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    newContactData: {
+      title: '',
+      rating: ''
+    },
+    newContactModal: false
   }
   componentWillMount() {
-    axios.get('http://localhost:3000/contacts').then((response) => {
+    axios.get('http://localhost:3001/contacts').then((response) => {
       this.setState({
         contacts: response.data
       })
     })
   }
+
+  toggleNewContactModal() {
+    this.setState({
+      newContactModal: !this.state.newContactModal
+    })
+  }
+
+  addContact() {
+    axios.post('http://localhost:3001/contacts', this.state.newContactData).then((response) => {
+      let { contacts } = this.state;
+      contacts.push(response.data)
+      this.state({contacts, newContactModal: false, newContactData: {
+          title: '',
+          rating: ''
+        }})
+    })
+  }
+
   render() {
     let contacts = this.state.contacts.map((contact) => {
       return (
@@ -32,6 +55,33 @@ class App extends Component {
     })
     return (
       <div className="App container">
+
+        <Button color="primary" onClick={this.toggleNewContactModal.bind(this)}>Add Contact</Button>
+        <Modal isOpen={this.state.newContactModal} toggle={this.toggleNewContactModal.bind(this)}>
+          <ModalHeader toggle={this.toggleNewContactModal.bind(this)}>Add a new contact</ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <Label for="title">Title</Label>
+              <Input id="title" value={this.state.newContactData.title} onChange={(e) => {
+                let { newContactData } = this.state;
+                newContactData.title = e.target.value
+                this.setState({ newContactData })
+              }} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="rating">Rating</Label>
+              <Input id="rating" value={this.state.newContactData.rating} onChange={(e) => {
+                let { newContactData } = this.state;
+                newContactData.rating = e.target.value
+                this.setState({ newContactData })
+              }} />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.addContact.bind(this)}>Add contact</Button>{' '}
+            <Button color="secondary" onClick={this.toggleNewContactModal.bind(this)}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
         <Table>
           <thead>
             <tr>
@@ -49,4 +99,4 @@ class App extends Component {
     );
   }
 }
-  export default App;
+export default App;
