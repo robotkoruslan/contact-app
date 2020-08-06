c6a2724957b5c7408fe178a4e7b8af0c23014079
Ruslan Robotko <42251945+robotkoruslan@users.noreply.github.com>
2020-08-06 13:02:25 +0300

    Add edit
    Add edit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,13 @@ class App extends Component {
       title: '',
       rating: ''
     },
-    newContactModal: false
+    editContactData: {
+      id: '',
+      title: '',
+      rating: ''
+    },
+    newContactModal: false,
+    editContactModal: false
   }
   componentWillMount() {
     axios.get('http://localhost:3001/contacts').then((response) => {
@@ -27,18 +33,30 @@ class App extends Component {
       newContactModal: !this.state.newContactModal
     })
   }
+  toggleEditContactModal() {
+    this.setState({
+      editContactModal: !this.state.editContactModal
+    })
+  }
 
   addContact() {
     axios.post('http://localhost:3001/contacts', this.state.newContactData).then((response) => {
       let { contacts } = this.state;
-      contacts.push(response.data)
-      this.state({contacts, newContactModal: false, newContactData: {
+      contacts.push(response.data);
+      this.setState({contacts, newContactModal: false, newContactData: {
           title: '',
           rating: ''
         }})
     })
   }
-
+  updateContact(){
+    
+  }
+    editContact(id, title, rating){
+    this.setState({
+      editContact: {id, title, rating}
+    })  
+    }
   render() {
     let contacts = this.state.contacts.map((contact) => {
       return (
@@ -47,16 +65,17 @@ class App extends Component {
           <td>{contact.title}</td>
           <td>{contact.rating}</td>
           <td>
-            <Button color="success" size="sm" className="mr-2">Edit</Button>
-            <Button color="danger" size="sm">Delete</Button> */
+            <Button color="success" size="sm" className="mr-2" onClick={this.editContact.bind(this, contact.id, contact.title, contact.rating)}>Edit</Button>
+            <Button color="danger" size="sm">Delete</Button>
           </td>
         </tr>
       )
     })
     return (
       <div className="App container">
+        <h1>Contacts App</h1>
 
-        <Button color="primary" onClick={this.toggleNewContactModal.bind(this)}>Add Contact</Button>
+        <Button className='my-3' color="primary" onClick={this.toggleNewContactModal.bind(this)}>Add Contact</Button>
         <Modal isOpen={this.state.newContactModal} toggle={this.toggleNewContactModal.bind(this)}>
           <ModalHeader toggle={this.toggleNewContactModal.bind(this)}>Add a new contact</ModalHeader>
           <ModalBody>
@@ -82,6 +101,33 @@ class App extends Component {
             <Button color="secondary" onClick={this.toggleNewContactModal.bind(this)}>Cancel</Button>
           </ModalFooter>
         </Modal>
+
+<Modal isOpen={this.state.editContactModal} toggle={this.toggleEditContactModal.bind(this)}>
+          <ModalHeader toggle={this.toggleEditContactModal.bind(this)}>Edit a new contact</ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <Label for="title">Title</Label>
+              <Input id="title" value={this.state.editContactData.title} onChange={(e) => {
+                let { editContactData } = this.state;
+                editContactData.title = e.target.value
+                this.setState({ editContactData })
+              }} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="rating">Rating</Label>
+              <Input id="rating" value={this.state.editContactData.rating} onChange={(e) => {
+                let { editContactData } = this.state;
+                editContactData.rating = e.target.value
+                this.setState({ editContactData })
+              }} />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.updateContact.bind(this)}>Update contact</Button>{' '}
+            <Button color="secondary" onClick={this.toggleEditContactModal.bind(this)}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
         <Table>
           <thead>
             <tr>
@@ -100,3 +146,6 @@ class App extends Component {
   }
 }
 export default App;
+
+
+//41.03
